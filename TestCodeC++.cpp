441f2fa4796be5103f14b5c39074ad8288c643bd
Huyphung111 <huythelep@gmail.com>
2026-03-08 23:06:45 +0700

    Revert "among2"
    This reverts commit dfb86161e25ae0fe70fb35f7fa65d89ee86ffa31.

--- TestCodeC++.cpp
+++ TestCodeC++.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 int main()
 {
-	cout << "Test";
-    int x;
-    cout << "Hello Amonoggg!\n";
-    cout << "Nhap vao x:";
-	cin >> x;
-	cout << "So nguyen am " << x ;
-	cout << "Hello Amonog2g!\n";
+	cout << "XinChaoTuiCommit";
+    int n;
+    cout << "Hello World!\n";
+    cout << "Nhap vao n:";
+	cin >> n;
+	cout << "So nguyen am " << n ;
     return 0;
 }
 
