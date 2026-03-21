9a6cca15375bc3ca5dfd9a3f21a591cd0767f524
Huyphung111 <huythelep@gmail.com>
2026-03-21 11:38:06 +0700

    oki

--- TestCodeC++.cpp
+++ TestCodeC++.cpp
@@ -9,6 +9,11 @@ int main()
     int n;
     cout << "Nhap vao n:";
 	cin >> n;
+	do
+	{
+		cout << "Xin chao ban da toi c++";
+		cout << "Vui long nhap vao so khac 0 de tiep tuc hoac 0 de thoat:";
+	} while (n == 0);
 	cout << "So nguyen am " << n ;
 	cout << "Test";
     int x;
