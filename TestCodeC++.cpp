28b1dcb897b63e35492d40e3bd4fbe95fcc78a88
Huyphung111 <huythelep@gmail.com>
2026-03-21 11:45:14 +0700

    da cap nhat

--- TestCodeC++.cpp
+++ TestCodeC++.cpp
@@ -6,23 +6,6 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cout << "Nhap vao n:";
-	cin >> n;
-	do
-	{
-		cout << "Xin chao ban da toi c++";
-		cout << "Vui long nhap vao so khac 0 de tiep tuc hoac 0 de thoat:";
-	} while (n == 0);
-	cout << "So nguyen am " << n ;
-	cout << "Test";
-    int x;
-    cout << "Hello Amonoggg!\n";
-    cout << "Nhap vao x:";
-	cin >> x;
-	cout << "So nguyen am ";
-	cout << "Hello Amonog2g!\n";
-    return 0;
-	cout << "XinChaoTuiCommit";
+
 }
 
