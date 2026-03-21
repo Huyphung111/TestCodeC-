8568b807ee656ea09feece9fb77dcde0e6e05af0
Huyphung111 <huythelep@gmail.com>
2026-03-21 11:30:38 +0700

    Revert "ok"
    This reverts commit c7a98d6b688e33bd8b5b576ee5a840d136926240.

--- TestCodeC++.cpp
+++ TestCodeC++.cpp
@@ -9,10 +9,15 @@ int main()
     int n;
     cout << "Nhap vao n:";
 	cin >> n;
-	do
-	{
-		cout << "Xin chao ban da toi c++";
-		cout << "Vui long nhap vao so khac 0 de tiep tuc hoac 0 de thoat:";
-	} while (n == 0);
+	cout << "So nguyen am " << n ;
+	cout << "Test";
+    int x;
+    cout << "Hello Amonoggg!\n";
+    cout << "Nhap vao x:";
+	cin >> x;
+	cout << "So nguyen am " << x ;
+	cout << "Hello Amonog2g!\n";
+    return 0;
+	cout << "XinChaoTuiCommit";
 }
 
