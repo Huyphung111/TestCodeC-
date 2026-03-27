c3d910e1b463028be010d8ec8e5dfde3ca5edf5f
Huyphung111 <huythelep@gmail.com>
2026-03-27 18:51:58 +0700

    commit6
    Update function

--- TestCodeC++.cpp
+++ TestCodeC++.cpp
@@ -1,22 +1,42 @@
-// TestCodeC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
-	int n;
-	cout << "Enter a number: ";
-	cin >> n;
-	if (n % 2 == 0) {
-		cout << n << " is even." << endl;
-	}
-	else {
-		cout << n << " is odd." << endl;
-	}
-	//Heloo tui la phung tuan huy
+// Hàm kiểm tra số nguyên tố (prime number)
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
 }
 
-//Day la su thay doi trong code github
+int main() {
+    int n;
+    cout << "Nhap so luong phan tu: ";
+    cin >> n;
+
+    int x, sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Nhap so thu " << i << ": ";
+        cin >> x;
+
+        // Chẵn / lẻ
+        if (x % 2 == 0)
+            cout << x << " la so chan\n";
+        else
+            cout << x << " la so le\n";
+
+        // Nguyên tố
+        if (isPrime(x))
+            cout << x << " la so nguyen to\n";
+
+        sum += x;
+        cout << "-------------------\n";
+    }
+
+    cout << "Tong cac so: " << sum << endl;
+
+    return 0;
+}
